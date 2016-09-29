9d1f6c8ac81c2b544fd6e82b7815d96edbdc6714
thekarura <karura2310@yahoo.co.jp>
2016-09-30 07:34:16 +0900

    [add] add first call method file.

---
+++ src/main/java/net/tkarura/undergroundplus2/UnderGroundPlus2.java
@@ -0,0 +1,17 @@
+package net.tkarura.undergroundplus2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UnderGroundPlus2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+	
+    }
+
+    @Override
+    public void onDisable() {
+	
+    }
+
+}
