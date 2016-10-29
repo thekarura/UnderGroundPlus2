694833cfcc8e8d93f139e3fd7206a26c2414e81c
thekarura <karura2310@yahoo.co.jp>
2016-10-30 01:53:32 +0900

    [update] UnderGoundPlus2 を Singleton に変更

--- src/main/java/net/tkarura/undergroundplus2/UnderGroundPlus2.java
+++ src/main/java/net/tkarura/undergroundplus2/UnderGroundPlus2.java
@@ -4,13 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UnderGroundPlus2 extends JavaPlugin {
 
+    private static UnderGroundPlus2 instance;
+
     @Override
     public void onEnable() {
+
+	instance = this;
 	
+	this.getLogger().info("plugin has enable.");
+
     }
 
     @Override
     public void onDisable() {
+
+	this.getLogger().info("plugin has disabled.");
 	
     }
 
